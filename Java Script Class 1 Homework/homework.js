fdca52eb020356dae0f36cca2b75e1c5d73ba5c3
GregoriBavaro <93792282+GregoriBavaro@users.noreply.github.com>
2021-12-17 19:43:28 +0100

    Add files via upload

---
+++ Java Script Class 1 Homework/homework.js
@@ -0,0 +1,18 @@
+// Homework for Class 1 Java Script
+
+/*Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax-rate is 5%.*/
+
+let mobilePhone = 119.95;
+console.log(mobilePhone);
+let percentage = 5;
+console.log(percentage);
+
+
+let phones30 = mobilePhone * 30;
+console.log(phones30);
+
+let fraction = percentage / 100;
+console.log(fraction);
+
+let totalSum = fraction * phones30;
+console.log(totalSum);
