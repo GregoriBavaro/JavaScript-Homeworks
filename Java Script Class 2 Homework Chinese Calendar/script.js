025b2ec3846ad2071994c76e6557b24f41a57e64
GregoriBavaro <93792282+GregoriBavaro@users.noreply.github.com>
2021-12-20 10:09:51 +0100

    Add files via upload

---
+++ Java Script Class 2 Homework Chinese Calendar/script.js
@@ -0,0 +1,104 @@
+// Solution 1
+let year = prompt(`Enter year of birth:`);
+
+if((year - 4) % 12 == 0) {
+    window.alert('Your Chinese Zodiac sign is Rat');
+}
+else if((year - 4) % 12 == 1) {
+    window.alert('Your Chinese Zodiac sign is Ox');
+}
+else if((year - 4) % 12 == 2) {
+    window.alert('Your Chinese Zodiac sign is Tiger');
+}
+else if((year - 4) % 12 == 3) {
+    window.alert('Your Chinese Zodiac sign is Rabbit');
+}
+else if((year - 4) % 12 == 4) {
+    window.alert('Your Chinese Zodiac sign is Dragon');
+}
+else if((year - 4) % 12 == 5) {
+    window.alert('Your Chinese Zodiac sign is Snake');
+}
+else if((year - 4) % 12 == 6) {
+    window.alert('Your Chinese Zodiac sign is Horse');
+}
+else if((year - 4) % 12 == 7) {
+    window.alert('Your Chinese Zodiac sign is Goat');
+}
+else if((year - 4) % 12 == 8) {
+    window.alert('Your Chinese Zodiac sign is Monkey');
+}
+else if((year - 4) % 12 == 9) {
+    window.alert('Your Chinese Zodiac sign is Rooster');
+}
+else if((year - 4) % 12 == 10) {
+    window.alert('Your Chinese Zodiac sign is Dog');
+}
+else if((year - 4) % 12 == 11) {
+    window.alert('Your Chinese Zodiac sign is Pig');
+}
+else {
+    window.alert('Invalid Year');
+}
+
+
+// Solution 2 
+
+/*let inputYear = prompt('Enter year of Birth');
+let numberInput = parseInt (inputYear);
+let year = (inputYear - 4) % 12
+
+if (year === 0) {
+    console.log('Rat');
+    window.alert('Your Chinese Zodiac sign is Rat');
+}
+else if (year === 1) {
+    console.log('Ox');
+    window.alert('Your Chinese Zodiac sign is Ox');
+}   
+else if (year === 2) {
+    console.log('Tiger');
+    window.alert('Your Chinese Zodiac sign is Tiger');
+}
+else if (year === 3) {
+    console.log('Rabbit');
+    window.alert('Your Chinese Zodiac sign is Rabbit');
+}
+else if (year === 4) {
+    console.log('Dragon');
+    window.alert('Your Chinese Zodiac sign is Dragon');
+}
+else if (year === 5) {
+    console.log('Snake');
+    window.alert('Your Chinese Zodiac sign is Snake');
+}
+else if (year === 6) {
+    console.log('Horse');
+    window.alert('Your Chinese Zodiac sign is Horse');
+}
+else if (year === 7) {
+    console.log('Goat');
+    window.alert('Your Chinese Zodiac sign is Goat');
+}
+else if (year === 8) {
+    console.log('Monkey');
+    window.alert('Your Chinese Zodiac sign is Goat');
+}
+else if (year === 9) {
+    console.log('Roster');
+    window.alert('Your Chinese Zodiac sign is Monkey');
+}
+else if (year === 10) {
+    console.log('Dog');
+    window.alert('Your Chinese Zodiac sign is Rooster');
+}
+else if (year === 11) {
+    console.log('Pig');
+    window.alert('Your Chinese Zodiac sign is Dog');
+}
+else {
+    window.alert('Invalid Year');
+}
+*/
+
+
